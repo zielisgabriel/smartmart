d9fd3a79236f075896683bad9fdb7515ee2f92c3
zielisgabriel <josegabrielas0586@email.com>
2026-01-10 23:55:55 -0300

    refactor: replace generic ProductController with specific controllers for improved route handling in product_routes

--- backend/routes/product_routes.py
+++ backend/routes/product_routes.py
@@ -1,11 +1,20 @@
 from flask import Blueprint
-from controllers.product_controller import ProductController
+from controllers.products.list_product_controller import ListProductController
+from controllers.products.get_product_controller import GetProductController
+from controllers.products.create_product_controller import CreateProductController
+from controllers.products.update_product_controller import UpdateProductController
+from controllers.products.delete_product_controller import DeleteProductController
 
 product_routes = Blueprint("products", __name__)
-controller = ProductController()
 
-product_routes.add_url_rule("", view_func=controller.list, methods=["GET"])
-product_routes.add_url_rule("", view_func=controller.create, methods=["POST"])
-product_routes.add_url_rule("/<int:product_id>", view_func=controller.get, methods=["GET"])
-product_routes.add_url_rule("/<int:product_id>", view_func=controller.update, methods=["PUT"])
-product_routes.add_url_rule("/<int:product_id>", view_func=controller.delete, methods=["DELETE"])
+listProductController = ListProductController()
+getProductController = GetProductController()
+createProductController = CreateProductController()
+updateProductController = UpdateProductController()
+deleteProductController = DeleteProductController()
+
+product_routes.add_url_rule("/list", view_func=listProductController.listProduct, methods=["GET"])
+product_routes.add_url_rule("/create", view_func=createProductController.createProduct, methods=["POST"])
+product_routes.add_url_rule("/<int:product_id>", view_func=getProductController.getProduct, methods=["GET"])
+product_routes.add_url_rule("/update/<int:product_id>", view_func=updateProductController.updateProduct, methods=["PUT"])
+product_routes.add_url_rule("/delete/<int:product_id>", view_func=deleteProductController.deleteProduct, methods=["DELETE"])
