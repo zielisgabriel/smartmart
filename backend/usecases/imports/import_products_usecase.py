480fd9465bfc42e5f74856035ce76dfd61d78cb9
zielisgabriel <josegabrielas0586@email.com>
2026-01-11 00:24:23 -0300

    feat: implement ImportProductsUsecase for importing product data from CSV

---
+++ backend/usecases/imports/import_products_usecase.py
@@ -0,0 +1,48 @@
+from usecases.imports.import_model import ImportModel
+import pandas as pd
+from database import db
+from decimal import Decimal
+
+
+class ImportProductsUsecase(ImportModel):
+    def __init__(self):
+        super().__init__(expected_columns={
+            "id": int,
+            "name": str,
+            "description": str,
+            "price": Decimal,
+            "brand": str,
+            "category_id": int
+        })
+
+    def execute(self, file):
+        df = pd.read_csv(file)
+        df.columns = df.columns.str.strip().str.lower()
+        csv_columns = set(df.columns)
+
+        self.validate_columns(csv_columns)
+
+        cols_to_keep = list(self.expected_keys)
+        df = df[cols_to_keep]
+
+        if df.isnull().any().any():
+            raise ValueError("Existem células vazias no arquivo. Todos os campos são obrigatórios.")
+
+        try:
+            with db.engine.begin() as connection:
+                df.to_sql(
+                    name="products",
+                    con=connection,
+                    if_exists='append',
+                    index=False,
+                    method='multi',
+                    chunksize=1000
+                )
+        except Exception as e:
+            if 'unique constraint' in str(e).lower():
+                raise ValueError("Erro de duplicação: Alguns IDs do arquivo já existem no banco de dados.")
+            if 'foreign key' in str(e).lower() or 'integrity' in str(e).lower():
+                raise ValueError("Erro de integridade: Verifique se todas as categorias referenciadas existem.")
+            raise e
+
+        return len(df)
