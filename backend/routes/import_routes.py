1e80a631d534e8f765506cbb321d191d4733f164
zielisgabriel <josegabrielas0586@email.com>
2026-01-10 23:55:47 -0300

    refactor: replace generic ImportController with specific controllers for improved clarity in import routes

--- backend/routes/import_routes.py
+++ backend/routes/import_routes.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
-from controllers.import_controller import ImportController
+from controllers.imports.import_categories_controller import ImportCategoriesController
+from controllers.imports.import_products_controller import ImportProductsController
+from controllers.imports.import_sales_controller import ImportSalesController
 
 import_routes = Blueprint("import", __name__)
-controller = ImportController()
 
-import_routes.add_url_rule("/categories", view_func=controller.import_categories, methods=["POST"])
-import_routes.add_url_rule("/products", view_func=controller.import_products, methods=["POST"])
-import_routes.add_url_rule("/sales", view_func=controller.import_sales, methods=["POST"])
+importCategoriesController = ImportCategoriesController()
+importProductsController = ImportProductsController()
+importSalesController = ImportSalesController()
+
+import_routes.add_url_rule("/categories", view_func=importCategoriesController.importCategories, methods=["POST"])
+import_routes.add_url_rule("/products", view_func=importProductsController.importProducts, methods=["POST"])
+import_routes.add_url_rule("/sales", view_func=importSalesController.importSales, methods=["POST"])
