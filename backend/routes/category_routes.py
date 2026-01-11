a5a9d63504f4ec5bac71602d4095ac41c8348d64
zielisgabriel <josegabrielas0586@email.com>
2026-01-10 23:55:30 -0300

    refactor: replace CategoryController with specific controllers for improved route handling in category_routes

--- backend/routes/category_routes.py
+++ backend/routes/category_routes.py
@@ -1,11 +1,20 @@
 from flask import Blueprint
-from controllers.category_controller import CategoryController
+from controllers.category.list_category_controller import ListCategoryController
+from controllers.category.get_category_controller import GetCategoryController
+from controllers.category.create_category_controller import CreateCategoryController
+from controllers.category.update_category_controller import UpdateCategoryController
+from controllers.category.delete_category_controller import DeleteCategoryController
 
 category_routes = Blueprint("categories", __name__)
-controller = CategoryController()
 
-category_routes.add_url_rule("", view_func=controller.list, methods=["GET"])
-category_routes.add_url_rule("", view_func=controller.create, methods=["POST"])
-category_routes.add_url_rule("/<int:category_id>", view_func=controller.get, methods=["GET"])
-category_routes.add_url_rule("/<int:category_id>", view_func=controller.update, methods=["PUT"])
-category_routes.add_url_rule("/<int:category_id>", view_func=controller.delete, methods=["DELETE"])
+listCategoryController = ListCategoryController()
+getCategoryController = GetCategoryController()
+createCategoryController = CreateCategoryController()
+updateCategoryController = UpdateCategoryController()
+deleteCategoryController = DeleteCategoryController()
+
+category_routes.add_url_rule("/list", view_func=listCategoryController.listCategory, methods=["GET"])
+category_routes.add_url_rule("/create", view_func=createCategoryController.createCategory, methods=["POST"])
+category_routes.add_url_rule("/<int:category_id>", view_func=getCategoryController.getCategory, methods=["GET"])
+category_routes.add_url_rule("/<int:category_id>", view_func=updateCategoryController.updateCategory, methods=["PUT"])
+category_routes.add_url_rule("/<int:category_id>", view_func=deleteCategoryController.deleteCategory, methods=["DELETE"])
