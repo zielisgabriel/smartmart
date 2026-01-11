b261056d72c069153b1a4c9bb5c82ae895c7c809
zielisgabriel <josegabrielas0586@email.com>
2026-01-10 23:56:00 -0300

    refactor: replace generic SaleController with specific controllers for improved route handling in sale_routes

--- backend/routes/sale_routes.py
+++ backend/routes/sale_routes.py
@@ -1,11 +1,23 @@
 from flask import Blueprint
-from controllers.sale_controller import SaleController
+from controllers.sales.list_sale_controller import ListSaleController
+from controllers.sales.get_sale_controller import GetSaleController
+from controllers.sales.create_sale_controller import CreateSaleController
+from controllers.sales.delete_sale_controller import DeleteSaleController
+from controllers.sales.stats_sale_controller import StatsSaleController
+from controllers.sales.monthly_stats_sale_controller import MonthlyStatsSaleController
 
 sale_routes = Blueprint("sales", __name__)
-controller = SaleController()
 
-sale_routes.add_url_rule("", view_func=controller.list, methods=["GET"])
-sale_routes.add_url_rule("", view_func=controller.create, methods=["POST"])
-sale_routes.add_url_rule("/stats", view_func=controller.stats, methods=["GET"])
-sale_routes.add_url_rule("/<int:sale_id>", view_func=controller.get, methods=["GET"])
-sale_routes.add_url_rule("/<int:sale_id>", view_func=controller.delete, methods=["DELETE"])
+listSaleController = ListSaleController()
+getSaleController = GetSaleController()
+createSaleController = CreateSaleController()
+deleteSaleController = DeleteSaleController()
+statsSaleController = StatsSaleController()
+monthlyStatsSaleController = MonthlyStatsSaleController()
+
+sale_routes.add_url_rule("", view_func=listSaleController.listSales, methods=["GET"])
+sale_routes.add_url_rule("", view_func=createSaleController.createSale, methods=["POST"])
+sale_routes.add_url_rule("/stats", view_func=statsSaleController.getStats, methods=["GET"])
+sale_routes.add_url_rule("/stats/monthly", view_func=monthlyStatsSaleController.getMonthlyStats, methods=["GET"])
+sale_routes.add_url_rule("/<int:sale_id>", view_func=getSaleController.getSale, methods=["GET"])
+sale_routes.add_url_rule("/<int:sale_id>", view_func=deleteSaleController.deleteSale, methods=["DELETE"])
