96195289e6039082763cb8fe73f2bac7432c8d3e
zielisgabriel <josegabrielas0586@email.com>
2026-01-14 18:26:57 -0300

    feat: add updateProductService for updating product details

---
+++ frontend/src/services/products/update-product-service.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { fetchClient } from "@/lib/fetch-client";
+import { revalidateTag } from "next/cache";
+
+interface UpdateProductData {
+  id: number
+  name: string
+  description: string
+  price: number
+  category_id: number
+  brand: string
+}
+
+export async function updateProductService(data: UpdateProductData) {
+  const response = await fetchClient({
+    path: `/api/products/update/${data.id}`,
+    init: {
+      body: JSON.stringify(data),
+      method: "PUT"
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error("Erro ao atualizar produto");
+  }
+
+  revalidateTag("products", { expire: 0 });
+
+  return response.json();
+}
