1f8a0b9e8dd832fb5df500c2ba088dd72d8ab26d
zielisgabriel <josegabrielas0586@email.com>
2026-01-14 18:26:18 -0300

    feat: add createProductService for creating new product entries

---
+++ frontend/src/services/products/create-product-service.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { fetchClient } from "@/lib/fetch-client"
+import { revalidateTag } from "next/cache"
+
+interface CreateProductData {
+  name: string
+  description: string
+  price: number
+  category_id: number
+  brand: string
+}
+
+export async function createProductService(data: CreateProductData) {
+  const response = await fetchClient({
+    path: "/api/products/create",
+    init: {
+      method: "POST",
+      body: JSON.stringify(data)
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error("Erro ao criar produto");
+  }
+
+  revalidateTag("products", { expire: 0 });
+
+  return response.json()
+}
