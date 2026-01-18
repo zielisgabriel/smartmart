7cbf97f8115df3268d85e5374ee697bc2b0a7344
zielisgabriel <josegabrielas0586@email.com>
2026-01-18 13:53:11 -0300

    feat: remove GetCategoryController and GetCategoryUsecase for codebase cleanup

--- backend/usecases/category/get_category_usecase.py
+++
@@ -1,8 +0,0 @@
-from repositories.category_repository import CategoryRepository
-
-class GetCategoryUsecase():
-  def __init__(self):
-    self.repository = CategoryRepository()
-
-  def execute(self, categoryId):
-    return self.repository.find_by_id(categoryId)
