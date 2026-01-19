f0bf94f5816a631577b6aacda52496aa6ff488e5
zielisgabriel <josegabrielas0586@email.com>
2026-01-19 00:12:03 -0300

    feat: enhance importProductsService with file validation and error handling

--- frontend/src/services/products/import-products-service.ts
+++ frontend/src/services/products/import-products-service.ts
@@ -1,23 +1,52 @@
 "use server";
 
+import { RequiredActionException } from "@/exceptions/required-action-exception";
 import { fetchClient } from "@/lib/fetch-client";
+import { ActionType } from "@/types/action-type";
 import { revalidateTag } from "next/cache";
 
-export async function importProductsService(formData: FormData) {
-  const response = await fetchClient({
-    path: "/api/import/products",
-    init: {
-      method: "POST",
-      body: formData
-    },
-  });
+export async function importProductsService(_prevState: ActionType, formData: FormData): Promise<ActionType> {
+  try {
+    const file = formData.get("file");
+    if (!file || !(file instanceof File)) {
+      return {
+        status: "ERROR",
+        message: "Arquivo inválido ou não enviado.",
+        timestamp: Date.now()
+      }
+    }
 
-  if (!response.ok) {
-    const error = await response.json()
-    throw new Error(error.error || "Erro ao importar produtos");
+    await fetchClient({
+      path: "/api/import/products",
+      init: {
+        method: "POST",
+        body: formData
+      },
+    });
+
+    revalidateTag("products", { expire: 0 });
+
+    return {
+      status: "SUCCESS",
+      message: "Produtos importados com sucesso!",
+      timestamp: Date.now()
+    }
+  } catch (error) {
+    if (error instanceof RequiredActionException) {
+      const actions = error.actions
+
+      return {
+        status: "ERROR",
+        message: error.description,
+        actions: actions,
+        timestamp: Date.now()
+      }
+    }
+
+    return {
+      status: "ERROR",
+      message: "Erro interno ao processar importação",
+      timestamp: Date.now()
+    }
   }
-
-  revalidateTag("products", { expire: 0 });
-
-  return response.json()
 }
