b7576ec2d928ac092ff6acd6c04491946c990fa3
zielisgabriel <josegabrielas0586@email.com>
2026-01-19 00:09:22 -0300

    feat: import RequiredActionException in ListCategoryController for enhanced error handling

--- backend/controllers/category/list_category_controller.py
+++ backend/controllers/category/list_category_controller.py
@@ -1,5 +1,6 @@
 from usecases.category.list_category_usecase import ListCategoryUsecase
 from flask import jsonify
+from exceptions.required_action_exception import RequiredActionException
 
 class ListCategoryController:
     def __init__(self):
