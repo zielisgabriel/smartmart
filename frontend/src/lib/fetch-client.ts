78f2e100e4e7ad7d1238141ed2ef7792c0caae13
zielisgabriel <josegabrielas0586@email.com>
2026-01-19 00:11:57 -0300

    feat: enhance fetchClient with error handling and required action verification

--- frontend/src/lib/fetch-client.ts
+++ frontend/src/lib/fetch-client.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { HttpException } from "@/exceptions/http-exception";
+import { RequiredActionException } from "@/exceptions/required-action-exception";
+
 interface FetchClientProps {
   path: string,
   host?: string,
@@ -9,7 +12,33 @@ interface FetchClientProps {
 export async function fetchClient({ path, host, init }: FetchClientProps) {
   const API_URL = host ?? process.env.API_URL;
   
-  return await fetch(`${API_URL + path}`, {
+  const response = await fetch(`${API_URL + path}`, {
     ...init
   });
+
+  if (response.ok) {
+    return response;
+  }
+
+  let errorData;
+  try {
+    errorData = await response.json();
+  } catch (error) {
+    errorData = null;
+  }
+
+  switch (response.status) {
+    case 409:
+      verifyRequiredAction(await errorData);
+    default:
+      throw new HttpException(500, "Internal Error", "Internal Error");
+  }
+}
+
+function verifyRequiredAction(errorData?: RequiredActionException) {
+  if (errorData && errorData.actions) {
+    throw new RequiredActionException(errorData.actions, errorData.description, errorData.name);
+  }
+
+  throw new HttpException(500, "Internal Error", "Internal Error");
 }
