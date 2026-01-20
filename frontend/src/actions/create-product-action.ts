7892af0cbe98f703395b50ea86f63e5a70c14dac
zielisgabriel <josegabrielas0586@email.com>
2026-01-19 23:11:49 -0300

    feat: implement createProductAction for product creation with validation

---
+++ frontend/src/actions/create-product-action.ts
@@ -0,0 +1,84 @@
+"use server";
+
+import { BadRequestException } from "@/exceptions/bad-request-exception";
+import { createProductService } from "@/services/products/create-product-service";
+import { ActionType } from "@/types/action-type";
+import z from "zod";
+
+const productSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Nome deve ter pelo menos 3 caracteres")
+    .max(100, "Nome deve ter no máximo 100 caracteres"),
+  description: z
+    .string()
+    .min(10, "Descrição deve ter pelo menos 10 caracteres")
+    .max(500, "Descrição deve ter no máximo 500 caracteres"),
+  price: z
+    .string()
+    .refine((val) => !isNaN(parseFloat(val)) && parseFloat(val) > 0, {
+      message: "Preço deve ser um número positivo",
+    }),
+  category_id: z.string().min(1, "Selecione uma categoria"),
+  brand: z
+    .string()
+    .min(2, "Marca deve ter pelo menos 2 caracteres")
+    .max(50, "Marca deve ter no máximo 50 caracteres"),
+});
+
+type ProductSchemaType = z.infer<typeof productSchema>;
+
+export async function createProductAction(_: unknown, formData: FormData): Promise<ActionType<FormData>> {
+  const data: ProductSchemaType = {
+    name: formData.get("name") as string,
+    description: formData.get("description") as string,
+    price: formData.get("price") as string,
+    category_id: formData.get("category_id") as string,
+    brand: formData.get("brand") as string
+  }
+
+  const schemaValidation = productSchema.safeParse(data);
+
+  if (!schemaValidation.success) {
+    return {
+      status: "ERROR",
+      message: "Erro de validação",
+      errors: schemaValidation.error.issues,
+      timestamp: Date.now(),
+      payload: formData
+    }
+  }
+
+  try {
+    await createProductService({
+      name: data.name,
+      description: data.description,
+      brand: data.brand,
+      category_id: Number(data.category_id),
+      price: Number(data.price)
+    });
+
+    return {
+      status: "SUCCESS",
+      message: "Produto criado com sucesso!",
+      payload: formData,
+      timestamp: Date.now()
+    }
+  } catch (error: any) {
+    if (error instanceof BadRequestException) {
+      return {
+        status: "ERROR",
+        message: error.description,
+        timestamp: Date.now(),
+        payload: formData
+      }
+    }
+
+    return {
+      status: "ERROR",
+      message: "Erro desconhecido",
+      timestamp: Date.now(),
+      payload: formData
+    }
+  }
+}
