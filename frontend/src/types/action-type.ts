da57066fdec1b75d6a659fae2b2981da7f41adf3
zielisgabriel <josegabrielas0586@email.com>
2026-01-19 21:14:04 -0300

    feat: enhance ActionType interface to support generic payload and optional errors

--- frontend/src/types/action-type.ts
+++ frontend/src/types/action-type.ts
@@ -1,12 +1,15 @@
-export interface ActionType {
+export interface ActionType<T = null> {
   status: "IDLE" | "SUCCESS" | "ERROR";
   message: string;
+  payload: T;
   actions?: string[];
   timestamp: number;
+  errors?: any[];
 }
 
 export const INITIAL_TYPE: ActionType = {
   status: "IDLE",
   message: "",
+  payload: null,
   timestamp: 0
 }
