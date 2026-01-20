cacc1a3769cb143ffed6ae3edb73e9c8fdfdbb08
zielisgabriel <josegabrielas0586@email.com>
2026-01-19 21:14:00 -0300

    refactor: simplify importProductsService by removing error handling and unnecessary parameters

--- frontend/src/services/products/import-products-service.ts
+++ frontend/src/services/products/import-products-service.ts
@@ -1,52 +1,20 @@
 "use server";
 
-import { RequiredActionException } from "@/exceptions/required-action-exception";
 import { fetchClient } from "@/lib/fetch-client";
-import { ActionType } from "@/types/action-type";
 import { revalidateTag } from "next/cache";
 
-export async function importProductsService(_prevState: ActionType, formData: FormData): Promise<ActionType> {
-  try {
-    const file = formData.get("file");
-    if (!file || !(file instanceof File)) {
-      return {
-        status: "ERROR",
-        message: "Arquivo inválido ou não enviado.",
-        timestamp: Date.now()
-      }
+export async function importProductsService(formData: FormData) {
+  console.log("importProductsService | FormData: ", formData);
+
+  const response = await fetchClient({
+    path: "/api/import/products",
+    init: {
+      body: formData,
+      method: "POST"
     }
+  });
 
-    await fetchClient({
-      path: "/api/import/products",
-      init: {
-        method: "POST",
-        body: formData
-      },
-    });
-
-    revalidateTag("products", { expire: 0 });
-
-    return {
-      status: "SUCCESS",
-      message: "Produtos importados com sucesso!",
-      timestamp: Date.now()
-    }
-  } catch (error) {
-    if (error instanceof RequiredActionException) {
-      const actions = error.actions
-
-      return {
-        status: "ERROR",
-        message: error.description,
-        actions: actions,
-        timestamp: Date.now()
-      }
-    }
-
-    return {
-      status: "ERROR",
-      message: "Erro interno ao processar importação",
-      timestamp: Date.now()
-    }
-  }
+  revalidateTag("products", { expire: 0 })
+  
+  return response;
 }
